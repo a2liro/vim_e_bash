47ccdf083c5c0acfa75da379a15222db5705644c
André Liro <andreliro1945@gmail.com>
2017-11-22 08:27:34 -0300

    criando repositório

---
+++ bundle/teste.php
@@ -0,0 +1,34 @@
+<?php
+echo teste
+echo d
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <div class="col">
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+<div class="col-md-12">
+
+    <ul>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+    </ul>
+
+</div>
+</body>
+</html>
